ef1756a955c7e250fa5ae3cb45973d3b9910ea91
YeeChuen <tychuen98@gmail.com>
2024-03-14 03:32:31 -0500

    Added Text Embedding

--- README.md
+++ README.md
@@ -10,6 +10,11 @@
 Follow the steps below for installation:
 1. ```git clone https://github.com/YeeChuen/RAG_model_project```
 2. ```pip install -r requirements.txt```
-3. ```python -m spacy download en_core_web_sm```
+4. Download Ollama on https://github.com/ollama/ollama
+5. ```ollama run llama2```
+6. ```ollama pull llama2```
+7. Create a free account on Pinecone, https://app.pinecone.io/
+8. Create an index in your Pinecone account
+9. Add the you Pinecone API key to this project. (TODO: how to add?)
 
 ### Usage
--- upload.py
+++ upload.py
@@ -1,13 +1,23 @@
 # Author(s): Yee
 '''
 Usage:
-    python3 upload.py --pdf_file=Correcting_Diverse_Factual_Errors_paper.pdf
+    python3 upload.py --pdf_file=Correcting_Diverse_Factual_Errors_paper.pdf --pinecone_key=7f89efd5-ec0d-49df-b35c-7be9459539a8
 '''
 #_____
 # imports
 import argparse
+# document loader
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import SpacyTextSplitter
+# text splitter
+from langchain_text_splitters import CharacterTextSplitter
+# text embedding
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+# store to a vector DB
+from pinecone import Pinecone
+
+# testing
+from langchain_community.llms import Ollama
 
 #_____
 # variables
@@ -17,37 +27,45 @@ from langchain_text_splitters import SpacyTextSplitter
 def getArgument():
     parser = argparse.ArgumentParser(description="Upload a PDF file to the RAG model.")
     parser.add_argument("--pdf_file", help=":Path to the PDF file to upload", required=True)
+    parser.add_argument("--pinecone_key", help=":The API key in your Pinecone console at https://app.pinecone.io/", required=True)
     return parser.parse_args()
 
 def loadPdf(pdf_file):
     loader = PyPDFLoader(pdf_file) # <-- basic pypdf, not going to extract images. (Does not work with my old local Mac)
     pages = loader.load_and_split()
     #print(len(pages)) # <-- development and debug purposes
-    #print(pages[0].page_content) # <-- development and debug purposes
-
-    document_list = []
-
-    for i in range(len(pages)):
-        document_list.append(pages[i].page_content)
-
-    return "\n".join(document_list)
+    #print(pages[0]) # <-- development and debug purposes
+    return pages
 
 def textSplitter(document):
-    text_splitter = SpacyTextSplitter(chunk_size=200)
-    split_texts = text_splitter.split_text(document)
+    text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=0)
+    split_texts = text_splitter.split_documents(document)
     #print(split_texts[0]) # <-- development and debug purposes
     #print(len(split_texts)) # <-- development and debug purposes
-
     return split_texts
+
+def textEmbedding(split_texts):
+    embeddings = OllamaEmbeddings()
+    vector = FAISS.from_documents(split_texts, embeddings)
+    print(vector) # <-- development and debug purposes
+    print(len(vector)) # <-- development and debug purposes
+
 
 def main():
     args = getArgument()
-    print("Loading PDF file...")
+    pc = Pinecone(api_key=args.pinecone_key)
 
+    print("Spawnning a temporary vector db... (TODO)")
+    #index = pc.Index("tempdb") # <-- TODO: implement pinecone free version, Starter Index.
+
+    print("Loading PDF file...")
     document = loadPdf(args.pdf_file)
-    #print(document) # <-- development and debug purposes.
-    textSplitter(document)
-    
+    print('Spltting document...')
+    split_texts = textSplitter(document)
+    print('Embedding document...')
+    textEmbedding(split_texts)
+
+    #pc.delete_index("tempdb") # <-- TODO: delete the starter index created for testing Upload.py
 
 #_____
 # main
