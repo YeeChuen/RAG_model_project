b5236f2afc8c202f8627aa438f6c894ef9c66048
YeeChuen <tychuen98@gmail.com>
2024-03-14 02:32:02 -0500

    Added Text Splitter using Spacy

--- upload.py
+++ upload.py
@@ -1,12 +1,13 @@
 # Author(s): Yee
 '''
 Usage:
-    python upload.py --pdf_file=Correcting_Diverse_Factual_Errors_paper.pdf
+    python3 upload.py --pdf_file=Correcting_Diverse_Factual_Errors_paper.pdf
 '''
 #_____
 # imports
 import argparse
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import SpacyTextSplitter
 
 #_____
 # variables
@@ -18,13 +19,35 @@ def getArgument():
     parser.add_argument("--pdf_file", help=":Path to the PDF file to upload", required=True)
     return parser.parse_args()
 
+def loadPdf(pdf_file):
+    loader = PyPDFLoader(pdf_file) # <-- basic pypdf, not going to extract images. (Does not work with my old local Mac)
+    pages = loader.load_and_split()
+    #print(len(pages)) # <-- development and debug purposes
+    #print(pages[0].page_content) # <-- development and debug purposes
+
+    document_list = []
+
+    for i in range(len(pages)):
+        document_list.append(pages[i].page_content)
+
+    return "\n".join(document_list)
+
+def textSplitter(document):
+    text_splitter = SpacyTextSplitter(chunk_size=200)
+    split_texts = text_splitter.split_text(document)
+    #print(split_texts[0]) # <-- development and debug purposes
+    #print(len(split_texts)) # <-- development and debug purposes
+
+    return split_texts
+
 def main():
     args = getArgument()
     print("Loading PDF file...")
+
+    document = loadPdf(args.pdf_file)
+    #print(document) # <-- development and debug purposes.
+    textSplitter(document)
     
-    loader = PyPDFLoader(args.pdf_file, extract_images=True)
-    pages = loader.load()
-    print(pages[0])
 
 #_____
 # main
