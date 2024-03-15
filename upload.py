a3a125f319a97c3d675c81720b8a94871a08e30f
YeeChuen <tychuen98@gmail.com>
2024-03-14 19:51:50 -0500

    Code cleanup

--- upload.py
+++ upload.py
@@ -13,14 +13,10 @@ from langchain_community.document_loaders import PyPDFLoader
 # text splitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 # text embedding
-from langchain_community.embeddings import OllamaEmbeddings
 from sentence_transformers import SentenceTransformer
 
 # store to a vector DB
 from pinecone import Pinecone
-
-# testing
-from langchain_community.llms import Ollama
 
 #_____
 # variables
@@ -33,77 +29,29 @@ def getArgument():
     parser = argparse.ArgumentParser(description="Upload a PDF file to the RAG model.")
     parser.add_argument("--pdf_file", help=":Path to the PDF file to upload", required=True)
     parser.add_argument("--pinecone_key", help=":The API key in your Pinecone console at https://app.pinecone.io/", default = '')
-    parser.add_argument("--openai_key", help=":The API key for open-ai", default = '')
+    #parser.add_argument("--openai_key", help=":The API key for open-ai", default = '')
     return parser.parse_args()
 
 def loadPdf(pdf_file):
     print("Loading PDF file...")
     loader = PyPDFLoader(pdf_file) # <-- basic pypdf, not going to extract images. (Does not work with my old local Mac)
     document = loader.load_and_split()
-    #print(len(document)) # <-- development and debug purposes
-    #print(document[0]) # <-- development and debug purposes
     return document
 
 def textSplitter(document):
     print('Spltting document...')
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=0)
     split_texts = text_splitter.split_documents(document)
-    print(split_texts[0]) # <-- development and debug purposes
-    print(split_texts[0].page_content) # <-- development and debug purposes
-    print(split_texts[0].metadata["source"]) # <-- development and debug purposes
-    print(split_texts[0].metadata["page"]) # <-- development and debug purposes
-    #print(split_texts[1]) # <-- development and debug purposes
-    #print(split_texts[2]) # <-- development and debug purposes
-    #print(split_texts[3]) # <-- development and debug purposes
-    #print(split_texts[4]) # <-- development and debug purposes
-    #print(len(split_texts)) # <-- development and debug purposes
     return split_texts
 
 def textEmbedding(split_texts):
     print('Embedding document...')
-    #embeddings = OllamaEmbeddings(model="llama2") # <-- depreciated, too long
-
     model = SentenceTransformer("all-MiniLM-L6-v2") # <-- smaller embedding model
 
-    '''
-    # testing 
-    print("Testing on random text")
-    random_text = "test ollama embedding result"
-    print(random_text) # <-- development and debug purposes
-    query_result = embeddings.embed_query(random_text) # <-- vector size of 4096
-    #print(query_result) # <-- development and debug purposes
-    print(len(query_result)) # <-- development and debug purposes
-    '''
-    '''
-    # testing 
-    print("Testing on 1 of the split_text")
-    print(split_texts[0]) # <-- development and debug purposes
-    query_result = embeddings.embed_query(split_texts[0]) # <-- vector size of 4096
-    #print(query_result) # <-- development and debug purposes
-    print(len(query_result)) # <-- development and debug purposes
-    '''
-    print("Embedding the whole of split_texts")
     vectors = []
-    #debug = 0
     for text in tqdm(split_texts):
-        #if debug == 5: break
-        #vectors.append(embeddings.embed_query(text))
-        #print(text.page_content)
         vector = model.encode([text.page_content]) 
-        #print(vector[0]) # <-- development and debug purposes
-        #print(len(vector[0])) # <-- development and debug purposes
-
         vectors.append(vector[0])
-
-        #debug += 1
-
-    #print(vectors) # <-- development and debug purposes
-    #print(len(vectors)) # <-- development and debug purposes
-    #print(len(vectors[0])) # <-- development and debug purposes
-    #print(len(vectors[1])) # <-- development and debug purposes
-    #print(len(vectors[2])) # <-- development and debug purposes
-    #print(len(vectors[3])) # <-- development and debug purposes
-    #print(len(vectors[4])) # <-- development and debug purposes
 
     return vectors
 
@@ -130,25 +78,14 @@ def upsert(index_db, api_key, vectors, split_texts):
 
     index.upsert(vectors = upserts_list)
 
-    '''
-    index.upsert(
-    vectors=[ # <-- whenever we use the same 'id', the data get replaced.
-        {"id": "A", "values": [0.8, 0.1, 0.1, 0.1], "metadata": {'goal': 'testing'}},
-        {"id": "B", "values": [0.5, 0.2, 0.2, 0.2], "metadata": {'goal': 'testing'}},
-        {"id": "C", "values": [0.3, 0.9, 0.3, 0.3], "metadata": {'goal': 'testing'}},
-        {"id": "D", "values": [0.4, 0.4, 0.7, 0.4], "metadata": {'goal': 'testing'}}
-    ]
-    )
-    '''
-
 def main():
     args = getArgument()
-
 
     document = loadPdf(args.pdf_file)
     split_texts = textSplitter(document)    
     vectors = textEmbedding(split_texts)
 
+    # upload to pinecone db
     upsert("upload-db", args.pinecone_key, vectors, split_texts)
 
 #_____
